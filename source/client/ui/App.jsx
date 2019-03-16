963546f744341d159f03a369e6cf92d58f6a46a8
KirillTkachuk <kirill1997>
2019-03-16 19:12:28 +0300

    Connect titles to cells by id

--- source/client/ui/App.jsx
+++ source/client/ui/App.jsx
@@ -23,23 +23,23 @@ class App extends React.Component {
         name: "Table Name 1",
         titles: [
           {
-            id: "STRAIGHT THROUGH",
+            id: "1",
             name: "STRAIGHT THROUGH",
           },
           {
-            id: "WITHDRAW 100 FROM MASTERCARD",
+            id: "2",
             name: "WITHDRAW 100 FROM MASTERCARD",
           },
           {
-            id: "WITHDRAW 70",
+            id: "3",
             name: "WITHDRAW 70",
           },
           {
-            id: "WITHDRAW 30 FROM MASTERCARD",
+            id: "4",
             name: "WITHDRAW 30 FROM MASTERCARD",
           },
           {
-            id: "WITHDRAW 10 FROM MASTERCARD",
+            id: "5",
             name: "WITHDRAW 10 FROM MASTERCARD",
           }
         ],
@@ -57,22 +57,27 @@ class App extends React.Component {
                 cells: [
                   {
                     id: "122121",
+                    titleID: "1",
                     name: "VISA",
                   },
                   {
                     id: "622362",
+                    titleID: "2",
                     name: "MASTERCARD",
                   },
                   {
                     id: "3g4g3",
+                    titleID: "3",
                     name: "",
                   },
                   {
                     id: "g3g3g3g",
+                    titleID: "4",
                     name: "",
                   },
                   {
                     id: "h34h4h3",
+                    titleID: "5",
                     name: "",
                   }
                 ],
@@ -86,22 +91,27 @@ class App extends React.Component {
                 cells: [
                   {
                     id: "bdssdbsdb",
+                    titleID: "1",
                     name: "",
                   },
                   {
                     id: "nre9ern",
+                    titleID: "2",
                     name: "",
                   },
                   {
                     id: "lkxcls4",
+                    titleID: "3",
                     name: "",
                   },
                   {
                     id: "dfm;sdfl;sd;6765765",
+                    titleID: "4",
                     name: "",
                   },
                   {
                     id: "dfds.f.ds/f,.5645645",
+                    titleID: "5",
                     name: "",
                   },
                 ],
@@ -122,22 +132,27 @@ class App extends React.Component {
                 cells: [
                   {
                     id: "99",
+                    titleID: "1",
                     name: "VISA",
                   },
                   {
                     id: "968t7tt",
+                    titleID: "2",
                     name: "MASTERCARD",
                   },
                   {
                     id: "msdasl;dmsa;l6m6",
+                    titleID: "3",
                     name: "",
                   },
                   {
                     id: "sdfsd44,,ww",
+                    titleID: "4",
                     name: "",
                   },
                   {
                     id: "gdfgdf,g'df,gfd6463",
+                    titleID: "5",
                     name: "",
                   }
                 ],
@@ -151,22 +166,27 @@ class App extends React.Component {
                 cells: [
                   {
                     id: "tqqt22",
+                    titleID: "1",
                     name: "MASTERCARD",
                   },
                   {
                     id: "reg445645",
+                    titleID: "2",
                     name: "MIR",
                   },
                   {
                     id: "d;v,.ds;v,ds;fds;fdsf3",
+                    titleID: "3",
                     name: "",
                   },
                   {
                     id: "plekekmvdkmdlks;6",
+                    titleID: "4",
                     name: "",
                   },
                   {
                     id: "c,'sc,scopdmme7778",
+                    titleID: "5",
                     name: "",
                   }
                 ],
@@ -180,22 +200,27 @@ class App extends React.Component {
                 cells: [
                   {
                     id: "2jjky9",
+                    titleID: "1",
                     name: "MASTERCARD PLATINUM",
                   },
                   {
                     id: "d;sa',d;sad;'asd,a'sd,",
+                    titleID: "2",
                     name: "",
                   },
                   {
                     id: "g;lds;glsdl;m666",
+                    titleID: "3",
                     name: "",
                   },
                   {
                     id: "ckwkfmwepfmwepfwef75",
+                    titleID: "4",
                     name: "",
                   },
                   {
                     id: "dmfklsdfsdfldsfsd;0797",
+                    titleID: "5",
                     name: "",
                   }
                 ],
@@ -209,23 +234,28 @@ class App extends React.Component {
                 cells: [
                   {
                     id: "dasmdas;lmdas;lmd;asdmas6",
+                    titleID: "1",
                     name: "TEST1",
                   },
                   {
                     id: "as,,f,g,,74",
-                    name: "",
-                  },
-                  {
-                    id: "sdflsdf;ds,fdsl;35353",
-                    name: "",
-                  },
-                  {
-                    id: "fsdf,sd,fl;dsf,;sd9876",
+                    titleID: "2",
                     name: "",
                   },
                   {
                     id: "kwnfkwenflkcmvkler5",
+                    titleID: "5",
                     name: "TEST2",
+                  },
+                  {
+                    id: "sdflsdf;ds,fdsl;35353",
+                    titleID: "3",
+                    name: "",
+                  },
+                  {
+                    id: "fsdf,sd,fl;dsf,;sd9876",
+                    titleID: "4",
+                    name: "",
                   }
                 ]
               },
@@ -244,22 +274,27 @@ class App extends React.Component {
                 cells: [
                   {
                     id: "lmsdl,fl;d,sf,sdf;sd,762b",
+                    titleID: "1",
                     name: "",
                   },
                   {
                     id: "ddsgsdt6llfdlg;,df",
+                    titleID: "2",
                     name: "",
                   },
                   {
                     id: "dnfldsn6je6lse5-5e",
+                    titleID: "3",
                     name: "",
                   },
                   {
                     id: "mg;sdmgksdmgksld9494949--4i4i4m",
+                    titleID: "4",
                     name: "",
                   },
                   {
                     id: "fdsmfndsmfkldsmf74849454",
+                    titleID: "5",
                     name: "",
                   }
                 ]
@@ -272,22 +307,27 @@ class App extends React.Component {
                 cells: [
                   {
                     id: "fdfndsljfdslf8dsf8ds",
+                    titleID: "1",
                     name: "",
                   },
                   {
                     id: "fdsfmdskfmdskf993393",
+                    titleID: "2",
                     name: "",
                   },
                   {
                     id: "efkskf;lfdfds-44",
+                    titleID: "3",
                     name: "",
                   },
                   {
                     id: "dsfjsdklflkdsfds-345345",
+                    titleID: "4",
                     name: "",
                   },
                   {
                     id: "dfdsfmdsmf;dslfdsf--404404d",
+                    titleID: "5",
                     name: "",
                   }
                 ]
@@ -299,7 +339,22 @@ class App extends React.Component {
     ],
   };
 
+  sortCells = (cells, titles) => {
+    let arr = [];
+
+    titles.map(title => {
+      return cells.map(cell => {
+
+        if (title.id === cell.titleID) arr.push(cell)
+
+      })
+    });
+
+    return arr;
+  };
+
   render() {
+
     return (
       <div>
         {
@@ -334,7 +389,8 @@ class App extends React.Component {
                                 <tr key={prop.id}>
                                   <th scope="row" style={{paddingLeft: prop.level * 15}}>{prop.name}</th>
                                   {
-                                    prop.cells.map(cell => {
+
+                                    this.sortCells(prop.cells, item.titles).map(cell => {
                                       return (
                                         <td key={cell.id}>{cell.name}</td>
                                       )
